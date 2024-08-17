3842578d9b6f2918005c6f03094c5d1aa36d696f
Sota ito <sota-ito@digeon.co>
2024-08-18 04:34:16 +0900

    記事本文ページの作成とボタン改修

--- app/src/components/ViewMoreButton.tsx
+++ app/src/components/ViewMoreButton.tsx
@@ -8,8 +8,11 @@ type Props = {
 export const ViewMoreButton = ({ to, className }: Props) => {
   return (
     <div className={`flex justify-center ${className}`}>
-      <Link href={to} className="border-2 rounded-md py-4 px-24 border-black">
-        もっと見る
+      <Link
+        href={to}
+        className="view-more-button border-2 rounded-md py-4 px-24 border-black"
+      >
+        <span>もっと見る</span>
       </Link>
     </div>
   );
