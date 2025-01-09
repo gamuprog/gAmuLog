7b3513e6255894a62d543e812bc37787886b417c
gamuprog <gamuprog.contact@gmail.com>
2025-01-10 01:05:06 +0900

    記事タイトルCSS変更

--- app/src/components/post/PostTitle.tsx
+++ app/src/components/post/PostTitle.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostTitle({ children }: Props) {
   return (
-    <h1 className="text-xl md:text-3xl lg:text-4xl md:font-semibold tracking-tight leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="!text-xl md:!text-3xl md:font-semibold tracking-tight leading-tight md:leading-none mb-12 text-center md:text-left">
       {children}
     </h1>
   );
