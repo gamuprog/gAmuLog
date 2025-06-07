e40283c88e258c463607d03e550fc13169d0a530
gamuprog <gamuprog.contact@gmail.com>
2025-06-08 04:45:53 +0900

    delete MDX ;;

--- app/src/mdx-components.tsx
+++
@@ -1,6 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  // ひとまず素通し。後で独自 <CodeBlock> などをここで差し替えられる
-  return { ...components };
-}
